a2401dd5ba33d991ab14a07ca7c2f8c1e0e4b54e
hallefsantos <hallefsantos@gmail.com>
2021-03-23 10:40:19 -0300

    Add Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white">
-      <p>Header</p>
+    <header className="pt-3 sm:pt-8 pb-8">
+      <Link href="/">
+        <div className="table text-2xl cursor-pointer">
+          <strong>Github</strong>
+          <span className="inline-block ml-2 font-light">Jobs</span>
+        </div>
+      </Link>
     </header>
   )
 }
