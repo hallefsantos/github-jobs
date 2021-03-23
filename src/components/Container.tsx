796dcae977dcec6cf91ab2c8c09b2957f528922c
hallefsantos <hallefsantos@gmail.com>
2021-03-23 11:54:29 -0300

    Review components

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,7 +3,11 @@ type ContainerProps = {
 }
 
 const Container = ({ children }: ContainerProps) => {
-  return <div className="px-5 max-w-7xl mx-auto">{children}</div>
+  return (
+    <div className="min-h-screen bg-gray-light">
+      <div className="px-3 max-w-7xl mx-auto">{children}</div>
+    </div>
+  )
 }
 
 export default Container
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="pt-3 sm:pt-8 pb-8">
       <Link href="/">
-        <div className="table text-2xl cursor-pointer">
+        <div className="table text-2xl cursor-pointer text-gray-dark">
           <strong>Github</strong>
           <span className="inline-block ml-2 font-light">Jobs</span>
         </div>
